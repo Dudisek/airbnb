1896a7736088cc89d6c5abd64f207af7f35f4708
Dudisek <peta.duda@gmail.com>
2016-01-16 18:23:43 +0800

    Disable scroll and add marker in Google Maps

--- app/assets/javascripts/g_map_show_listing.js
+++ app/assets/javascripts/g_map_show_listing.js
@@ -1,8 +1,15 @@
 // # MAP
 var map;
 function initMap() {
+	var myLatLng = {lat: gon.lat, lng: gon.lng};
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: gon.lat, lng: gon.lng},
-    zoom: 15
+    center: myLatLng,
+    zoom: 15,
+    scrollwheel: false
+  });
+
+  var marker = new google.maps.Marker({
+  position: myLatLng,
+  map: map
   });
 }
